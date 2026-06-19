7e36cc7e42949c4d5924661f34cd08f05fc40ffe
Omar <omartellezse@gmail.com>
2026-06-18 19:45:40 -0500

    Primer commit taller 2

---
+++ app/src/main/java/com/example/tallercontactos/adaptadores/ContactoAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.tallercontactos.adaptadores
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tallercontactos.R
+import com.example.tallercontactos.entidad.Contacto
+
+class ContactoAdapter(var datos: List<Contacto>): RecyclerView.Adapter<ContactoAdapter.ContactoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactoViewHolder {
+        val vista = LayoutInflater.from(parent.context).inflate(R.layout.item_contacto, parent, false)
+        return ContactoViewHolder(vista)
+    }
+
+    override fun onBindViewHolder(holder: ContactoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return datos.size
+    }
+
+    class ContactoViewHolder(vista: View): RecyclerView.ViewHolder(vista){
+        private var contenedor: View
+        private var nombre: TextView
+        private var direccion: TextView
+        init{
+            contenedor = vista
+            nombre = vista.findViewById(R.id.txtNombre)
+            direccion = vista.findViewById(R.id.txtDireccion)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tallercontactos/entidad/Contacto.kt
@@ -0,0 +1,3 @@
+package com.example.tallercontactos.entidad
+
+data class Contacto(var nombre: String, var telefono: String, var correo: String, var direccion: String)
